5d61bddfc30d8519bca531252c9ccd9d6179fca7
Nicole <nicole.arguedas@tecsup.edu.pe>
2023-04-11 16:09:23 -0500

    First commit

---
+++ src/main/java/com/tecsup/maven/sem03/prueba/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.maven.sem03.prueba;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/tecsup/maven/sem03/prueba/AppTest.java
@@ -0,0 +1,20 @@
+package com.tecsup.maven.sem03.prueba;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
